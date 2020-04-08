be2d7af89b6adfdecdbd3cada46b4d59169ee2aa
Thiago De Paula <62727794+ThDePaula@users.noreply.github.com>
2020-04-08 14:45:24 -0300

    Resolução do exercício

--- ex065.py
+++ ex065.py
@@ -1,11 +1,17 @@
 resp = 'S'
-soma = 0
-media = 0
-quant = 0
+soma = media = quant = maior = menor = 0
 while resp in 'S':
     n = int(input('Informe um valor: '))
     soma += n
     quant += 1
+    if quant == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n                
     resp = str(input('Deseja continuar? [N/s] ')).strip().upper()[0]
 med = soma / quant
-print('Média dos números: {}'.format(med))
+print('Você informou {} números e a média foi: {}'.format(quant, med))
+print('O maior valor: {} e menor {}'.format(maior, menor))
